b50b44b945d7575ecf89ce93495d939927c930a6
rookiehome7 <rookiehome7@gmail.com>
2019-06-17 06:25:08 +0100

    3

--- SIPPhoneClient/phonecontrol.py
+++ SIPPhoneClient/phonecontrol.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 broker_IP = "192.168.1.10"
 broker_Port = 1883 #Default Port
 
-# In string 
+# Bedsite Unit ID  string 
 patientID = "200"
 
 #How to write to anoter terminal
@@ -21,17 +21,20 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     
     message = str(msg.payload)
-    print("MQTT Message:" + message)
+    print("MQTT Msg:" + message)
     split_message = message.split(':')
 
+    # This bedsite unit get request from MQTT Message
     if(split_message[0] == patientID):
-        print ("This patient incoming message")
-        if (split_message[1] == "call 101"):
-            os.system("twinkle --call 101")
-        if (split_message[1] == "answer"):
+        if (split_message[1][0:4] == "call"):
+            phonenumber = split_message[1].split(' ')
+            os.system("twinkle --call " +  phonenumber[1] ) 
+        elif (split_message[1] == "answer"):
             os.system("twinkle --cmd answer")
-        if (split_message[1] == "bye"):
+        elif (split_message[1] == "bye"):
             os.system("twinkle --cmd bye")
+        else:
+            print("Invalid MQTT Incomming message")
 
 client = mqtt.Client()
 client.on_connect = on_connect
